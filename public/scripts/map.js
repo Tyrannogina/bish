ea1c5645a5838b70578d65b8eb265c0ac05a44c7
Luis Amez <luis.amez.vitoria@gmail.com>
2017-07-17 21:10:31 +0200

    Add map without markers

---
+++ public/scripts/map.js
@@ -0,0 +1,42 @@
+function startMap() {
+  var ironhackBCN = {
+  	lat: 41.3977381,
+  	lng: 2.190471916};
+  var map = new google.maps.Map(
+    document.getElementById('map'),
+    {
+      zoom: 15,
+      center: ironhackBCN
+    }
+  );
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      const user_location = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+      // Center map with user location
+      map.setCenter(user_location);
+
+      // Add a marker for your user location
+      // var IronHackBCNMarker = new google.maps.Marker({
+      //   position: {
+      //     lat: user_location.lat,
+      //     lng: user_location.lng
+      //   },
+      //   map: map,
+      //   title: "You are here"
+      // });
+
+    }, function () {
+      console.log('Error in the geolocation service.');
+    });
+  } else {
+    console.log('Browser does not support geolocation.');
+  }
+
+  directionsDisplay.setMap(map);
+}
+
+startMap();
