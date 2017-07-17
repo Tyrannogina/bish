26f982165baf389202144763cae407144d814581
Luis Amez <luis.amez.vitoria@gmail.com>
2017-07-17 12:26:47 +0200

    Create user Schema

--- models/user.js
+++ models/user.js
@@ -1,18 +1,44 @@
 const mongoose = require("mongoose");
 const Schema   = mongoose.Schema;
 
+const languages = ["en", "es", "fr", "de", "it", "el", "ru", "pt"];
+
 const userSchema = new Schema({
   	username: String,
   	password: String,
-  	role: {
-    	type: String,
-    	enum : ['EDITOR', 'ADMIN'],
-    	default : 'ADMIN'
-  	}
+    alias: String,
+    description: String,
+    languagesOffer: [{
+      type: String,
+      enum: languages,
+      required: true
+    }],
+    languagesWant: [{
+      type: String,
+      enum: languages,
+      required: true
+    }],
+    locations: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Location"
+    },
+    lastLogin: {
+      type: Date,
+      default: Date.now
+    }
+  	// role: {
+    // 	type: String,
+    // 	enum : ['EDITOR', 'ADMIN'],
+    // 	default : 'ADMIN'
+  	// }
 	}, {
   	timestamps: { createdAt: "created_at", updatedAt: "updated_at" }
 	}
 );
+
+userSchema.statics.login = function login(id, callback) {
+   return this.findByIdAndUpdate(id, { $set : { 'lastLogin' : Date.now() }}, { new : true }, callback);
+};
 
 const User = mongoose.model("User", userSchema);
 
